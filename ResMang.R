e08b3c1f8f9d9f2e0fbc1401d21a858ba7f8f814
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-07-03 16:38:58 -0600

    Added notes on how to change dS in forescast

--- ResMang.R
+++ ResMang.R
@@ -156,7 +156,7 @@ minReleaseApril<-function(day, volF){
 #forecast what the storage would be in s days given previous ∆ in S and make those changes over m days
 forecastS<-function(s,m,day){
   if (day > s+1){
-    dsdt= (stor[day] - stor[day-s])/s #calculate daily change in storage over past s days
+    dsdt= (stor[day] - stor[day-s])/s #calculate daily change in storage over past s days --> change this to 1) take average of dS over s days, 2) create weighted average
     storF[day] <<- (dsdt*m)+stor[day] #forecasted flow for today is the change in storage* m days subtracted form todays storage -- does that make sense??
   } else {storF[day]<<- stor[day]}
 }  
@@ -164,7 +164,7 @@ forecastS<-function(s,m,day){
 #update discharge, change in storage and day+1 storage
 evalS<- function(Qin, day, stor, maxS, Qmin,s,m){
   if (storF[day] >= maxS[day,m] && day > s+1){ #&& day <188 #this is max storage m days out
-    dsdtMax= (storF[day] - maxS[day,m])/m #max change in storage is m days out divided by s days?? this doesnt make sense
+    dsdtMax= (storF[day] - maxS[day,m])/m #max change in storage is m days out divided by m
     qo[day] <- Qmin[day] + (dsdtMax*v2f)
     flag = 'TRUE' #true we need to increase ramp rates to get rid of the water
   } else {qo[day] <- Qmin[day]
