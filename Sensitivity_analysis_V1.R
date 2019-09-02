4b4173ac11ad5e92b774001b21566c12d327e07d
Kendra Kaiser <kendra.kaiser@gmail.com>
2019-09-02 16:43:46 -0600

    Created function to evaluate each model run from LHS

--- Sensitivity_analysis_V1.R
+++ Sensitivity_analysis_V1.R
@@ -13,50 +13,72 @@ modelRun<-function(params){
 }
 
 #set parameters
+maxDays=28
 q.arg<- list(list("min"=1, "max"=maxDays), list("min"=1, "max"=maxDays))
 names(q.arg)<-c("s", "m")
 factors<-c("s", "m")
-maxDays=28
 
 
 #myLHS<-LHS(model=modelRun, factors, N=100, q='qdunif', q.arg, nboot=4)
-n=500
+n=5
 #create hypercubes with different parameter sets
 bothLHS <-LHS(model = NULL, factors, N=n, q='qdunif', q.arg, nboot=1)
 outB<-modelRun(bothLHS$data)
 
-
-
-
-#turn this into a functino that reads in LHS output
-#Days managers or model exceeded max storage or discharge limits
-Mod_exceedDate=matrix(data=NA, nrow = 60, ncol = 21)
-Mod_overQlim=matrix(data=NA, nrow = 110, ncol = 21)
-DaysOver_Mod=matrix(data=NA, nrow = 21, ncol = 3)
-colnames(DaysOver_Mod)<-c("ModOverS", "ModOverQ", "ModVolQ")
-
-for (wy in 1:21){
-  MaxStor<- results[[wy]][,1]
-  ModStor<- results[[wy]][,3]
-  ModQ<- results[[wy]][,5]
+#----------------------------------------------------------------------------
+# Re-organize output for analysis - all runs for each wy in one matrix
+#----------------------------------------------------------------------------
+cleanData<-function(LHSrun){
+  wy_Q<-lapply(1:21, matrix, data= NA, nrow=196, ncol=n)
+  wy_stor<-lapply(1:21, matrix, data= NA, nrow=196, ncol=n)
+  maxStor<-matrix(data= NA, nrow=196, ncol=21)
+  reps = seq(1, length(LHSrun), 21)
   
-  Mod_exceed <- which(ModStor > MaxStor)
-  ll=length(Mod_exceed)
-  if(ll>0){
-    Mod_exceedDate[1:ll, wy]<-Mod_exceed
+  count=0
+  for (i in 1:21){
+    for (j in 1:n){
+      wy_Q[[i]][,j]<- LHSrun[[reps[j]+count]][,5]
+      wy_stor[[i]][,j]<- LHSrun[[reps[j]+count]][,3]
+    }
+    count=count+1
+    maxStor[,i]<- LHSrun[[i]][,1]
   }
-  topped <- which(ModStor > maxAF)
-  lll<-length(topped)
-  if (lll>0){
-    days_topped[1:lll, wy]<-topped
-  }
- 
-  Mod_Q <- which(ModQ >qlim[1:196,2])
-  mll<-length(Mod_Q)
-  if (mll>0){
-    Mod_overQlim[1:mll, wy]<-Mod_Q
-    Mod_Vol_over_Qlim<-sum(ModQ[Mod_Q]-qlim[Mod_Q,2])
-  } else{Mod_Vol_over_Qlim<-0}
-  
-  DaysOver_Mod[wy,]<-c(ll,mll, Mod_Vol_over_Qlim)
+  out<-list(wy_Q, wy_stor, maxStor)
+  names(out)<-(c("Q", "stor", "maxStor"))
+  return(out)
 }
+
+both<-cleanData(outB)
+
+#turn this into a function that reads in LHS output
+#Days managers or model exceeded max storage or discharge limits
+
+exceeds <- function(cleanedData){
+  modEval<-lapply(1:21, matrix, data= NA, nrow=4, ncol=n)
+  
+  for (i in 1:21){
+    for (j in 1:n){
+      Q<-cleanedData$Q[[i]][,j]
+      S<-cleanedData$stor[[i]][,j]
+      maxS<-cleanedData$maxStor[,i]
+      rownames(modEval[[i]])<-c('DaysStor', 'VolStor', 'DaysQlim', 'VolQlim')
+      
+      daysSover<- which(S > maxS)
+      Sover=length(stor_exceed)
+      modEval[[i]]['DaysStor',j]<-Sover
+      if(Sover>0){
+        modEval[[i]]['VolStor',j]<-sum(S[daysSover]-maxS[daysSover])
+      }
+
+      daysQover <- which(Q >qlim[1:196,2])
+      Qover<-length(daysQover)
+      modEval[[i]]['DaysQlim',j]<-Qover
+      if (Qover>0){
+        modEval[[i]]['VolQlim',j]<-sum(Q[daysQover]-qlim[daysQover,2])
+      } else{modEval[[i]]['VolQlim',j]<-0}
+    }
+  }
+  return(modEval)
+}
+
+modEval<-exceeds(both)
